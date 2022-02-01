b14dad7b17eb084d842adcab4d7fa153a1d2c9bb
Geena Jackson <gjacks303@gmail.com>
2022-02-01 14:09:20 -0700

    Create test for a guess and card

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,16 +1,31 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Turn = require('../src/Turn.js');
+const Card = require('../src/Card.js');
 
 describe('Turn', function() {
 
   it('should be a function', function() {
+    const card = new Card();
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
   it('should instantiate a class', function() {
+    const card = new Card();
     const turn = new Turn();
     expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should store a guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.guess).to.equal('array');
+  });
+
+  it('should store a card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('array', card);
+    expect(turn.card).to.equal(card);
   });
 });
