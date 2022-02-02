76f7de6cf20be3ac79380f9f9b901cbc1e83688d
Geena Jackson <gjacks303@gmail.com>
2022-02-02 14:46:36 -0700

    Create test for function

---
+++ test/Round-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Round = require('../src/Round.js');
+const Card = require('../src/Card.js');
+
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Round([card1, card2, card3]);
+    expect(Round).to.be.a('function');
+  });
+
+  it('should instantiate a class', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const round = new Round([card1, card2, card3]);
+    expect(round).to.be.an.instanceOf(Round);
+  });
+
+});
