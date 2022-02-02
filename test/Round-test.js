02bffb721155cf0f824282b88cc1f7508b49c5c5
Geena Jackson <gjacks303@gmail.com>
2022-02-02 16:34:05 -0700

    Create test for deck in round

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Round = require('../src/Round.js');
 const Card = require('../src/Card.js');
-
+const Deck = require('../src/Deck.js');
+const Round = require('../src/Round.js');
 
 describe('Round', function() {
 
@@ -10,7 +10,8 @@ describe('Round', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Round([card1, card2, card3]);
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck)
     expect(Round).to.be.a('function');
   });
 
@@ -18,8 +19,19 @@ describe('Round', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const round = new Round([card1, card2, card3]);
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck)
     expect(round).to.be.an.instanceOf(Round);
   });
+
+  it('should have a deck', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck)
+    expect(round.deck).to.deep.equal(deck);
+  });
+
 
 });
