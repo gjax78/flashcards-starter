793cc91d213ea9735128805d60a7692475a67a43
Geena Jackson <gjacks303@gmail.com>
2022-02-02 17:09:34 -0700

    Create test to return current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,6 +3,8 @@ const expect = chai.expect;
 const Card = require('../src/Card.js');
 const Deck = require('../src/Deck.js');
 const Round = require('../src/Round.js');
+const Turn = require('../src/Turn.js');
+
 
 describe('Round', function() {
 
@@ -30,8 +32,18 @@ describe('Round', function() {
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck)
-    expect(round.deck).to.deep.equal(deck);
+    expect(round.deck).to.deep.equal(deck.cards);
   });
+
+  it('should return the current card being played', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck)
+    expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
+  });
+
 
 
 });
